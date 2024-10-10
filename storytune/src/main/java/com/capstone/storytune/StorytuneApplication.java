d50b35544fd4922d289b502ace3b2aec6c2b4837
sueunlee <sueun00700@gmail.com>
2024-10-10 17:17:05 +0900

    Initial Setting

---
+++ storytune/src/main/java/com/capstone/storytune/StorytuneApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.storytune;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorytuneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorytuneApplication.class, args);
+	}
+
+}
