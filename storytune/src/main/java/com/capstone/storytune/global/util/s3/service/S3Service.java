e4b20200f7d33022219061b27400c8ea21f51902
sueunlee <sueun00700@gmail.com>
2024-11-19 01:04:35 +0900

    FEAT : S3 config, service 구현

---
+++ storytune/src/main/java/com/capstone/storytune/global/util/s3/service/S3Service.java
@@ -0,0 +1,33 @@
+package com.capstone.storytune.global.util.s3.service;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class S3Service {
+    private final AmazonS3 amazonS3;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucketName;
+
+    public String uploadImage(MultipartFile file) throws IOException {
+        String fileName = UUID.randomUUID().toString();
+
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentLength(file.getSize());
+        metadata.setContentType(file.getContentType());
+
+        amazonS3.putObject(bucketName, fileName, file.getInputStream(), metadata);
+        return amazonS3.getUrl(bucketName, fileName).toString();
+    }
+}
